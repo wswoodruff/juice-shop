54772cea075f0623890b7d4cb681f0e56e4d7ab3
wswoodruff <williamswoodruff@gmail.com>
2021-08-21 02:15:23 -0400

    Add default for basket.Products
    Signed-off-by: wswoodruff <williamswoodruff@gmail.com>

--- routes/basket.ts
+++ routes/basket.ts
@@ -18,7 +18,11 @@ module.exports = function retrieveBasket () {
           const user = security.authenticatedUsers.from(req)
           return user && id && id !== 'undefined' && id !== 'null' && id !== 'NaN' && user.bid && user.bid != id // eslint-disable-line eqeqeq
         })
-        if (basket?.Products && basket.Products.length > 0) {
+
+        // Set Products default
+        basket.Products = basket.Products || []
+
+        if (basket.length) {
           for (let i = 0; i < basket.Products.length; i++) {
             basket.Products[i].name = req.__(basket.Products[i].name)
           }
